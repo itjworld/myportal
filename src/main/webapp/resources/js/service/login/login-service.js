e375b751c4bdbd5074fd28f7a341359eefdeb452
Ashish Jaiswal <Ashish Jaiswal@10.210.2.45>
2017-11-07 18:03:29 +0530

    add services files

---
+++ src/main/webapp/resources/js/service/login/login-service.js
@@ -0,0 +1,30 @@
+'use strict';
+angular.module('appPortal').factory('loginService', ['$http', '$q', function($http, $q){
+	var LOGIN_URI = '/myportal/authenticate';
+	var method='POST'; 
+	var headers= {"Content-Type": "application/x-www-form-urlencoded;charset=utf-8;","X-Login-Ajax-call": 'true'};
+	
+	    var factory = {login: login};
+	    return factory;
+	    
+	    function login(credentials) {
+	        var deferred = $q.defer();
+	        $http({
+	            method: method,
+	            url: LOGIN_URI,
+	            data: credentials,
+	            headers:headers
+	        })
+	        .then(function(response) {
+	        	if (response.status ==200) {
+	        		deferred.resolve(response.statusText);
+	            }else if (response.status ==203)  {
+	            	deferred.resolve(response.data);
+	            }
+	        },
+	        function(errResponse){
+	        	deferred.reject(errResponse);
+	        });
+	        return deferred.promise;
+	    }
+}]);
