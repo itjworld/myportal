c9bcf4f0bbc7937ba287d34f07913c3bf9f51a74
Ashish Jaiswal <Ashish Jaiswal@10.210.2.45>
2017-11-07 15:12:22 +0530

    Demo file

---
+++ src/main/java/com/gspann/utils/Password.java
@@ -0,0 +1,20 @@
+package com.gspann.utils;
+
+import org.springframework.security.crypto.bcrypt.BCrypt;
+
+public class Password {
+	private static int workload = 12;
+	
+	public static String hashPassword(String password_plaintext) {
+		String salt = BCrypt.gensalt(workload);
+		String hashed_password = BCrypt.hashpw(password_plaintext, salt);
+		return(hashed_password);
+	}
+	public static boolean checkPassword(String password_plaintext, String stored_hash) {
+		return BCrypt.checkpw(password_plaintext, stored_hash);
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(checkPassword("Ashish2","$2a$12$oDLAZpkhRXmDSOEqKGhNgO04d8ccgcFog4S9cgdRTfXbC13CVo8uy"));
+	}
+}
